d102f5f82360a96c4716598d3212649d927e3fc1
vats <shivamvats1610@gmail.com>
2025-07-30 13:56:23 +0530

    kjb

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 
 // Middleware
 app.use(cors({
-  origin: ["https://deploy-mern-1whq.vercel.app"],
+  origin: ["https://employee-tracker-backend-iota.vercel.app/"],
   methods: ["POST", "GET"],
   credentials: true
 }));
