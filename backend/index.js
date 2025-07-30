a52e1e1eb36fa08800b7a6fc5e5e974a808e888c
vats <shivamvats1610@gmail.com>
2025-07-30 15:37:16 +0530

    bb

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,8 @@ const app = express();
 const multer = require('multer');
 
 // Middleware
-app.use(cors({
-  origin: ["https://employee-tracker-ap2x.onrender.com/"],
-  methods: ["POST", "GET"],
-  credentials: true
-}));
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
