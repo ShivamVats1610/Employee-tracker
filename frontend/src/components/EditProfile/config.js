85b4d66aa40609113299a3f5b2fb46349892d5bb
vats <shivamvats1610@gmail.com>
2025-07-31 02:18:42 +0530

    hib

--- frontend/src/components/EditProfile/config.js
+++ frontend/src/components/EditProfile/config.js
@@ -1,3 +1,8 @@
-// src/config.js
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8082';
+let base = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8082';
+
+if (base.endsWith('/')) {
+  base = base.slice(0, -1);
+}
+
+const API_BASE_URL = base;
 export default API_BASE_URL;
